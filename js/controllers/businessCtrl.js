0916370bce08cc595859be1de92a1ddc7b2f6738
Matias <matias.benincasa@gmail.com>
2015-03-03 13:47:03 -0300

    Borrado de promociones

--- js/controllers/businessCtrl.js
+++ js/controllers/businessCtrl.js
@@ -17,6 +17,12 @@ controllers.controller('BusinessCtrl',[
             $scope.form.type = "create"
         };
 
+        $scope.business.delete = function(index){
+                //BusinessSvc.delete($scope.business.result[index]);
+                $scope.business.results.splice(index,1);
+                console.log(index);
+        }
+
         BusinessSvc.get(function(response){
             $scope.business.results = response
         })
